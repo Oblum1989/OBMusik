6cbe0474611b25c9bb57084420b3f78dd9c31761
Oblum1989 <oscar.blum@gmail.com>
2019-04-01 12:57:56 -0500

    se arregla vista de artistas y se crea link hacia vista show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Artist.create(name: "Marc Anthony",image_url: "https://revistavenezolana.com/wp-content/uploads/2016/03/marc-anthony2.jpg")
+Artist.create(name: "Adele",image_url: "https://static.emol.cl/emol50/Fotos/2015/12/09/file_20151209165831.jpg")
+Artist.create(name: "Marilyn Manson",image_url: "http://imagenpoblana.com/sites/default/files/noticia_img/_locoensex.jpg")
+Artist.create(name: "Christina Aguilera",image_url: "https://revista.dicelacancion.com/wp-content/uploads/2015/12/CRISTINA-AGUILERA.jpg")
+Artist.create(name: "Alejandro Sanz",image_url: "https://2.bp.blogspot.com/-epFydYaYSog/U1ASbrho8uI/AAAAAAAARUA/vWu_ZznXb68/s1600/Alejandro+Sanz+1.jpg")
+Artist.create(name: "Alejandro Fernandez",image_url: "http://lh3.ggpht.com/_u5nKIhvxGVk/TZah8QxTaoI/AAAAAAAAAP8/r7wAVAwTQEo/resize_gallery_thumb%5B3%5D.jpg?imgmax=800")
+Artist.create(name: "Thalia",image_url: "https://revista.dicelacancion.com/wp-content/uploads/2015/01/Las-divas-latinas-mas-famosas.jpg")
+Artist.create(name: "The Mills",image_url: "https://indiespot.es/wp-content/uploads/2013/09/killers.jpg")
+Artist.create(name: "The Killers",image_url: "http://www.radioacktiva.com/wp-content/uploads/2015/07/19997_g_0b46f0_imgg1.jpg")
+
